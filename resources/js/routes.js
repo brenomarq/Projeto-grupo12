caf6f8d88b76747ecb3e3aac23b8d6bc9b13eebe
brenomarq <brenomarques516@gmail.com>
2023-11-24 18:39:12 -0300

    Form added

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 
-const app= express()
+const app = express()
 
 
 function callbackServido () {
     console.log("Servidor rodando na porta 3000")
 }
+
 app.listen(3000)
