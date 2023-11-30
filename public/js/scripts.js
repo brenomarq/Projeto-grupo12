2c7d8e83165a762196a3498dc074981cfbaee6d3
brenomarq <brenomarques516@gmail.com>
2023-11-30 10:54:26 -0300

    profile modal completed

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,27 @@
 var simplemde = new SimpleMDE({ element: document.getElementById("create-post") });
+
+const openCreatePost = document.querySelector(".btn-criarpost");
+const closeCreatePost = document.querySelector(".btn-fechar");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+    [modal, fade].forEach((element) => element.classList.toggle("hide"));
+}
+
+[openCreatePost, closeCreatePost].forEach((element) => {
+    element.addEventListener("click", () => toggleModal());
+})
+
+/*
+const openModal = () => {
+    const modalContent = document.createElement("div");
+    modalContent.innerHTML = "
+        < textarea id = "markdownEditor" ></textarea >
+            <div class="botoes">
+                <button id="cancelPost">Cancelar</button>
+                <button id="savePost">Publicar</button>
+
+            </div>"
+}
+*/
