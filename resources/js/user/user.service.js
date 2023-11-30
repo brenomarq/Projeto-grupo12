5f270d930762d8c1c97aab310f26766d06d8137b
ogjoaoc <joaocarlosgoncalvesfilho@gmail.com>
2023-11-30 13:59:51 -0300

    refactor user services

--- resources/js/user/user.service.js
+++ resources/js/user/user.service.js
@@ -1,33 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-async function newUser(userData) {
-  try {
-    const user = await prisma.user.create({
-      data: userData,
-    });
-    return user;
-  } catch (error) {
-    throw new Error('Erro ao cadastrar usuário.');
-  }
-}
-
-async function updateUserPassword(userId, newPassword) {
-  try {
-    const updatedUser = await prisma.user.update({
-      where: {
-        id: userId,
-      },
-      data: {
-        password: newPassword,
-      },
-    });
-    return updatedUser;
-  } catch (error) {
-    throw new Error('Erro ao atualizar a senha do usuário.');
-  }
-}
-
 async function getUserByEmail(email) {
   try {
     const user = await prisma.user.findUnique({
@@ -42,7 +15,5 @@ async function getUserByEmail(email) {
 }
 
 module.exports = {
-  newUser,
-  updateUserPassword,
   getUserByEmail,
 };
